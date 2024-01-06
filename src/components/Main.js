8b7ceb38c54452d16d0d2fdaac53b8ab770b44c2
Ujjwal Kumar <ujjwalkumar9025@gmail.com>
2024-01-07 00:49:52 +0530

    updating share box

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,92 @@
-import React from 'react'
+import styled from "styled-components";
 
 const Main = () => {
   return (
-    <div>
-      hello from main
-    </div>
-  )
-}
+    <Container>
+      <ShareBox>
+        <div>
+          <img src="/images/user.svg" alt="" />
+          <button>Start a post</button>
+        </div>
+
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="" />
+            <span>Photo</span>
+          </button>
+
+          <button>
+            <img src="/images/video-icon.svg" alt="" />
+            <span>Video</span>
+          </button>
+
+          <button>
+            <img src="/images/event-icon.svg" alt="" />
+            <span>Event</span>
+          </button>
+
+          <button>
+            <img src="/images/article-icon.svg" alt="" />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background: white;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      line-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-top: 8px;
+      }
+
+      button{
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0,15);
+      }
+    }
+  }
+`;
 
 export default Main;
